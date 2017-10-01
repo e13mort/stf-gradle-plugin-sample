1d7faaf8195500d6f060ab6cc41c2e22f7bd9fd2
Pavel <imortan@gmail.com>
2017-10-01 20:49:06 +0300

    sample app files added

---
+++ app/src/main/java/com/github/e13mort/stfgradleplugin/sampleapp/MainActivity.java
@@ -0,0 +1,15 @@
+package com.github.e13mort.stfgradleplugin.sampleapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.e13mort.github.com.sampleapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
